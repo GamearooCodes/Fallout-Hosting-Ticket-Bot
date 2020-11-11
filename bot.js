f1f59e31d1bc9c40a2a7375ee1561f9fe8490b6b
Gamearoo Codes <gamearoo@animetopic.pro>
2020-11-11 12:40:57 -0500

    1.0

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const { Client } = require('discord.js');
 const client = new Client();
 const { MessageEmbed } = require('discord.js');
 
+cooldown = new Set();
+
 client.on('ready', async () => {
 	console.log('Bot Has Started!');
 });
@@ -11,8 +13,15 @@ client.on('message', async message => {
 	if (message.author.bot) return;
 	let dmembed = new MessageEmbed().setDescription(`Sorry We Don't Provide Dm Support.`).setColor('RED');
 	if (message.channel.type === 'dm') {
+		if (cooldown.has(message.author.id)) return;
 		if (message.author.bot) return;
-		return message.channel.send(dmembed);
+
+		message.channel.send(dmembed);
+		cooldown.add(message.author.id);
+		setTimeout(() => {
+			cooldown.delete(message.author.id);
+		}, 200000);
+		return;
 	}
 });
 
